349c692ed72f0f13193315f4f881bcfdf8b16d30
themathsdude <elyass2008@hotmail.fr>
2023-01-22 02:36:47 +0100

    interpolation sur [0, 1] et inv vandermonde 👨‍💻

--- matrix-inversion.py
+++ matrix-inversion.py
@@ -9,17 +9,32 @@ def interpolation(A, B, penteA, penteB):
                             [3 * B[0] ** 2, 2 * B[0], 1, 0]])
     sndmembre = np.array([A[1], B[1], penteA, penteB])
     sol = np.linalg.solve(vandermonde, sndmembre)
-    return sol
+    return sol, vandermonde
 
 
-A = np.array((0, 0))
-B = np.array((1, 1))
+diametreTube = 1e-1
+rayonTube = diametreTube / 2
+alpha = np.pi/4
+longueurTubeY = 1
+longueurTube1 = 1
+d0 = diametreTube / np.sin(alpha) - rayonTube / np.tan(alpha)
+hauteurOscillations = 2 * rayonTube
+nbOscillations = 5
+longueurOscillations = 1
+k = 2 * np.pi * nbOscillations / longueurOscillations
+pente = hauteurOscillations * k
+
+A = np.array((longueurTubeY + d0 + longueurTube1, rayonTube))
+B = np.array((longueurTubeY + d0 + longueurTube1 + rayonTube, 2*rayonTube))
 penteA = 0
-penteB = 1
+penteB = pente*(B[1]-A[1])/(B[0]-A[0])
 
-coeffs = interpolation(A, B, penteA, penteB)
+C = np.array((0, 0))
+D = np.array((1, 1))
 
-x = np.linspace(A[0], B[0], 100)
+coeffs, vand = interpolation(C, D, penteA, penteB)
+
+x = np.linspace(C[0], D[0], 100)
 
 
 def f(x, coeffs):
@@ -34,6 +49,6 @@ y = f(x, coeffs)
 
 plt. figure()
 plt.plot(x, y)
-plt.plot(x, penteA*(x - A[0]) + A[1])
-plt.plot(x, penteB*(x - B[0]) + B[1])
+# plt.plot(x, penteA*(x - A[0]) + A[1])
+# plt.plot(x, penteB*(x - B[0]) + B[1])
 plt.show()
